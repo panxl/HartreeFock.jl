1a421ecb0849c209cf07be15e5e7e8c07085e102
panxl <panxl@users.noreply.github.com>
2020-05-03 20:18:42 -0500

    Move all constants to one file

---
+++ src/constants.jl
@@ -0,0 +1,17 @@
+const ELEMENTS = [
+    "H", "He",
+    "Li", "Be", "B", "C", "N", "O", "F", "Ne",
+    "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar",
+    "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr",
+    "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe",
+    "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb",
+    "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi"
+    ]
+
+const ANGULAR_MOMENTUM = [
+    [(0,0,0)], #S
+    [(1,0,0), (0,1,0), (0,0,1)], #P
+    [(2,0,0), (0,2,0), (0,0,2), (1,1,0), (1,0,1), (0,1,1)], #D
+    ]
+
+const CODATA08_BOHR_TO_A = 0.5291772085936
--- src/parser.jl
+++ src/parser.jl
@@ -14,18 +14,6 @@ function parser(file::String)
     end
 end
 
-const ELEMENTS = [
-    "H", "He",
-    "Li", "Be", "B", "C", "N", "O", "F", "Ne",
-    "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar",
-    "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr",
-    "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe",
-    "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb",
-    "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi"
-    ]
-
-const CODATA08_BOHR_TO_A = 0.5291772085936
-
 function read_xyz(lines::Vector{String})
     n = parse(Int, lines[1])
     positions = Vector{Vec3{Float64}}(undef, n)
